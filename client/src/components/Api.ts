3c6574fb771c1a03da951ad01618e5742f556070
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-02-25 23:50:41 +0530

    updated axios api

--- client/src/components/Api.ts
+++ client/src/components/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.110.31.131:2358:2358",
+  baseURL: "http://3.110.31.131:2358",
   headers:{
     "Content-Type": "application/json"
   }
