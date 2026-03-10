02f6643e5c1e347302ede0a5537d1b8a3a722330
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-10 21:07:04 +0530

    Added enableMongoAuditing anotation

--- backend/backend/src/main/java/com/codevspace/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/codevspace/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.codevspace.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 public class BackendApplication {
 
