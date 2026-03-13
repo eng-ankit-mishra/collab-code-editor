3522bb0f96cfcaec34fa0f1e554f41372c88a0e6
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-13 22:07:17 +0530

    Updated Project Controller

--- backend/backend/src/main/java/com/codevspace/backend/controller/ProjectController.java
+++ backend/backend/src/main/java/com/codevspace/backend/controller/ProjectController.java
@@ -1,12 +1,10 @@
 package com.codevspace.backend.controller;
 
-import com.codevspace.backend.dto.CodeSaveRequest;
-import com.codevspace.backend.dto.InviteCollaboratorRequest;
-import com.codevspace.backend.dto.ProjectCreateRequest;
-import com.codevspace.backend.dto.ProjectDashboardResponse;
+import com.codevspace.backend.dto.*;
 import com.codevspace.backend.model.Project;
 import com.codevspace.backend.model.User;
 import com.codevspace.backend.repository.ProjectRepository;
+import com.codevspace.backend.service.CodeExecutionService;
 import com.codevspace.backend.service.ProjectService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+    private final CodeExecutionService codeExecutionService;
 
     @PostMapping
     public ResponseEntity<Project> createProject(@RequestBody ProjectCreateRequest request, @AuthenticationPrincipal User currentUser) {
@@ -49,6 +48,14 @@ public class ProjectController {
     public ResponseEntity<Void> saveCode(@PathVariable String projectId, @RequestBody CodeSaveRequest request, @AuthenticationPrincipal User currentUser) {
         projectService.saveCode(projectId,request.getCodeContent(),currentUser);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{projectId}/execute")
+    public ResponseEntity<CodeExecutionResponse> executeCode(@PathVariable String projectId,@RequestBody CodeExecutionRequest request,@AuthenticationPrincipal User currentUser){
+
+
+        CodeExecutionResponse response=codeExecutionService.executeCode(request);
+        return ResponseEntity.ok().body(response);
     }
 
 
