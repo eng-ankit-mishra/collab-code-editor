93b8aca61e25060cb9dd01d9013d0481eaff78bf
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-13 19:41:15 +0530

    Updated Project Controller

---
+++ backend/backend/src/main/java/com/codevspace/backend/controller/ProjectController.java
@@ -0,0 +1,55 @@
+package com.codevspace.backend.controller;
+
+import com.codevspace.backend.dto.CodeSaveRequest;
+import com.codevspace.backend.dto.InviteCollaboratorRequest;
+import com.codevspace.backend.dto.ProjectCreateRequest;
+import com.codevspace.backend.dto.ProjectDashboardResponse;
+import com.codevspace.backend.model.Project;
+import com.codevspace.backend.model.User;
+import com.codevspace.backend.repository.ProjectRepository;
+import com.codevspace.backend.service.ProjectService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/projects")
+@RestController
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody ProjectCreateRequest request, @AuthenticationPrincipal User currentUser) {
+        Project project= projectService.createProject(request,currentUser);
+        return ResponseEntity.ok().body(project);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getMyProjects(@AuthenticationPrincipal User currentUser) {
+        List<Project> list= projectService.getProject(currentUser.getId());
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping("{projectId}/collaborators")
+    public ResponseEntity<Project> addCollaborator(@PathVariable String projectId, @RequestBody InviteCollaboratorRequest request, @AuthenticationPrincipal User currentUser ) {
+        Project project=projectService.addCollaborator(projectId,request,currentUser);
+        return ResponseEntity.ok().body(project);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectDashboardResponse>> getProjects(@AuthenticationPrincipal User currentUser) {
+        return ResponseEntity.ok().body(projectService.getDashboardProjects(currentUser.getId()));
+    }
+
+    @PatchMapping("{projectId}/code")
+    public ResponseEntity<Void> saveCode(@PathVariable String projectId, @RequestBody CodeSaveRequest request, @AuthenticationPrincipal User currentUser) {
+        projectService.saveCode(projectId,request.getCodeContent(),currentUser);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
