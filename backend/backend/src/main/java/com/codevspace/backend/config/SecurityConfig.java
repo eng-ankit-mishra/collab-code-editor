788199331f77171b939394aadce5e5d204aaf04d
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-16 18:45:49 +0530

    Updated Cors Configuration

--- backend/backend/src/main/java/com/codevspace/backend/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/codevspace/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/api/auth/**","/oauth2/**","/login/**","/ws/**","/api/contact/**").permitAll()
