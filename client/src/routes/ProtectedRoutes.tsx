20cf21adf02adf7726ef7d7057176ae793035641
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-19 21:47:16 +0530

    updated session logic for protected routes

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -1,25 +1,18 @@
-// routes/ProtectedRoutes.tsx
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../features/auth/context/useAuth.tsx";
-import SplashScreen from "../components/loader/FullScreenLoader.tsx";
+//import SplashScreen from "../components/loader/FullScreenLoader.tsx";
 
 export default function ProtectedRoutes({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { session, loading } = useAuth();
+  const { session,} = useAuth();
 
-  // ⏳ Wait until Supabase finishes restoring session
-  if (loading) {
-    return <SplashScreen />;
-  }
 
-  // 🔐 Not authenticated
   if (!session) {
     return <Navigate to="/login" replace />;
   }
 
-  // ✅ Authenticated
   return <>{children}</>;
 }
