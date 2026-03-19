76ef3befd9d9bf875ed36c86d72ca61723ce4259
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-19 21:34:32 +0530

    Updated the apiClient for global api handling

---
+++ client/src/api/apiClient.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers:{
+        "Content-Type": "application/json",
+    }
+})
+
+apiClient.interceptors.request.use(config => {
+
+        if (!config.url.includes('/api/auth/')) {
+            let token = localStorage.getItem("token");
+            if (token) {
+                token = token.replace(/['"]+/g, '').trim();
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+
+        return config;
+    },
+    error => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+    response=>response
+    ,error =>{
+        if(error.response && error.response.status===401){
+            localStorage.removeItem("token");
+            window.location.href="/login";
+        }
+        return Promise.reject(error)
+    } )
+
+
+export default apiClient;
