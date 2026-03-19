3291247ccd09f2271fca97d2a97114c0bb23396e
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-19 21:46:43 +0530

    handled project service api handling

---
+++ client/src/services/projectService.js
@@ -0,0 +1,15 @@
+import apiClient from "../api/apiClient.js";
+
+const projectService={
+    createProject:({name,description,language,codeContent})=>apiClient.post("/api/projects",{name,description,language,codeContent}).then(res=>res.data),
+    getAllProject:()=>apiClient.get("/api/projects").then(res=>res.data),
+    getProjectById:(id)=>apiClient.get(`/api/projects/${id}`).then(res=>res.data),
+    saveCode:(projectId,codeContent)=>apiClient.patch(`/api/projects/${projectId}/code`,{codeContent}).then(res=>res.data),
+    executeCode:(projectId,{languageId,sourceCode,stdin})=>apiClient.post(`/api/projects/${projectId}/execute`,{languageId,sourceCode,stdin}).then(res=>res.data),
+    sharedProject:()=>apiClient.get("/api/projects/shared").then(res=>res.data),
+    inviteUser:(projectId,email,role)=>apiClient.post(`/api/projects/${projectId}/invite`,{email,role}).then(res=>res.data),
+    getAllInvitations:()=>apiClient.get("/api/projects/invitations").then(res=>res.data),
+    respondToInvitation:(projectId,accept)=>apiClient.post(`/api/projects/${projectId}/invitations/respond`,{accept}).then(res=>res.data),
+}
+
+export default projectService;
