006b6a3b4b005de61b6c521b8ebf859e7b840c6d
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-20 20:26:25 +0530

    Removed legacy node backend

--- server/src/config/mailer.js
+++
@@ -1,6 +0,0 @@
-// server/config/mailer.js
-import sgMail from "@sendgrid/mail";
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-export default sgMail;
--- server/src/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendContactMessage } from "../models/Contact.controller.js";
-
-const router = Router();
-
-router.post("/", sendContactMessage);
-
-export default router;
