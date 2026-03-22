47a4b163c3dc1194f7e1fc1f29f85809a7031cec
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-03-22 20:16:27 +0530

    Added user id field

--- backend/backend/src/main/java/com/codevspace/backend/service/UserService.java
+++ backend/backend/src/main/java/com/codevspace/backend/service/UserService.java
@@ -14,9 +14,10 @@ public class UserService {
 
     public UserDetailResponse getUserDetail(User currentUser) {
 
-        User user = userRepository.findById(currentUser.getId()).orElseThrow(()->new IllegalArgumentException("Project Not Found"));
+        User user = userRepository.findById(currentUser.getId()).orElseThrow(()->new IllegalArgumentException("User Not Found"));
 
         return UserDetailResponse.builder()
+                .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
                 .avatarURL(user.getAvatarUrl())
