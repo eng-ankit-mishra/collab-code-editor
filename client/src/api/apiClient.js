9b7e345cba730f4653a6a9e129d57ee5476936c6
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-05-01 11:11:34 +0530

    Updated the api URL

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: "",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://collab-code-editor-backend-ybyk.onrender.com",
     headers:{
         "Content-Type": "application/json",
     }
